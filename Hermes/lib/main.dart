9b9ab989c94058f57f99c1b088cd9d00ba376369
ubernerd117 <sourab.shaiks@gmail.com>
2023-03-31 20:39:59 -0400

    hello there

--- Hermes/lib/main.dart
+++ Hermes/lib/main.dart
@@ -1,1 +1,27 @@
-//the main file for dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Hermes());
+}
+
+class Hermes extends StatelessWidget {
+  const Hermes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              title: const Text('Hermes'),
+            ),
+            body: Container(
+              child: const Text('Hello There!'),
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Colors.grey,
+              height: 100,
+              width: 100,
+            )));
+  }
+}
